f12b8ca60c1885ce19152d6065cc2d9ce8ea05df
Kareem Elgendy <kxreem@gmail.com>
2020-12-18 14:47:15 -0500

    Added support for multiple products

--- main.py
+++ main.py
@@ -20,44 +20,51 @@ def main():
         prod_id = product['product']['id'] # Product ID
         prod_options = product['product']['variants'] # List of product variants
 
-        target_prod = get_prod_type() # Product type
-        var_id = get_variant_id(prod_options, target_prod) # 
-        available = check_availability(prod_url, prod_id, var_id)
-        cart_url = get_cart_link(prod_url, var_id)
+        try:
+            target_prod = get_prod_type() # Product type
+            var_id = get_variant_id(prod_options, target_prod) # 
+            available = check_availability(prod_url, prod_id, var_id)
+            cart_url = get_cart_link(prod_url, var_id)
+            product_type = target_prod[0]
 
-        product_type = target_prod[0]
+            # Product in stock
+            if available:
+                print('\n-------- Product in stock --------')
 
-        # Product in stock
-        if available:
-            print('\n-------- Product in stock --------')
+                # Opening cart in browser
+                print('\nAdding product to cart, one moment.')
+                webbrowser.open_new(cart_url)
+                print('Task completed.\n')
 
-            # Opening cart in browser
-            print('\nAdding product to cart, one moment.')
-            webbrowser.open_new(cart_url)
-            print('Task completed.\n')
+                # No products being watched
+                if prod_dict.is_empty():
+                    quit()
+                else: 
+                    watch_products(profile_dict, prod_dict, prod_title)
 
-            # No products being watched
-            if prod_dict.is_empty():
-                quit()
-            else: 
-                watch_products(profile_dict, prod_dict, prod_title)
+            # Product not in stock
+            else:
+                print('\n-------- Product out of stock --------')
+                
+                if get_watch_option():
+                    prod_dict.new_product(prod_title) ## Adding product to watchlist    
+                    prod_dict.set_val(prod_title, 'Product URL', prod_url)
+                    prod_dict.set_val(prod_title, 'Product ID', prod_id)
+                    prod_dict.set_val(prod_title, 'Variant ID', var_id)
+                    prod_dict.set_val(prod_title, 'Cart URL', cart_url)
 
-        # Product not in stock
-        else:
-            print('\n-------- Product out of stock --------')
-            
-            if get_watch_option():
-                prod_dict.new_product(prod_title) ## Adding product to watchlist    
-                prod_dict.set_val(prod_title, 'Product URL', prod_url)
-                prod_dict.set_val(prod_title, 'Product ID', prod_id)
-                prod_dict.set_val(prod_title, 'Variant ID', var_id)
-                prod_dict.set_val(prod_title, 'Cart URL', cart_url)
+                    if get_auto_checkout(profile_dict):
+                        create_profile(profile_dict, prod_dict, prod_title)
 
-                if get_auto_checkout(profile_dict):
-                    create_profile(profile_dict, prod_dict, prod_title)
+                        if get_new_prod(prod_dict):
+                            main()
+        
+        except:
+            print('\nError occured.')
+            print('Make sure all entries are valid.')
 
-                    if get_new_prod(prod_dict):
-                        main()
+            if get_new_prod(prod_dict):
+                main()
 
         if not prod_dict.is_empty():
             watch_products(profile_dict, prod_dict, prod_title)
