389f5aba68591fa9339a74471dc6afc27eb3309a
Kareem Elgendy <kxreem@gmail.com>
2020-12-18 14:31:33 -0500

    Added support for multiple products

--- main.py
+++ main.py
@@ -8,66 +8,70 @@ from inputP import *
 from utilitiesP import *
 from watch import *
 
+# Main program
+def main():
+    # Getting the url from the user
+    prod_url = input('\nInsert Product URL: ')
+    product = get_product(prod_url)
+
+    # Validate URL
+    if product != False:
+        prod_title = product['product']['title'] # Product Title
+        prod_id = product['product']['id'] # Product ID
+        prod_options = product['product']['variants'] # List of product variants
+
+        target_prod = get_prod_type() # Product type
+        var_id = get_variant_id(prod_options, target_prod) # 
+        available = check_availability(prod_url, prod_id, var_id)
+        cart_url = get_cart_link(prod_url, var_id)
+
+        product_type = target_prod[0]
+
+        # Product in stock
+        if available:
+            print('\n-------- Product in stock --------')
+
+            # Opening cart in browser
+            print('\nAdding product to cart, one moment.')
+            webbrowser.open_new(cart_url)
+            print('Task completed.\n')
+
+            # No products being watched
+            if prod_dict.is_empty():
+                quit()
+            else: 
+                watch_products(profile_dict, prod_dict, prod_title)
+
+        # Product not in stock
+        else:
+            print('\n-------- Product out of stock --------')
+            
+            if get_watch_option():
+                prod_dict.new_product(prod_title) ## Adding product to watchlist    
+                prod_dict.set_val(prod_title, 'Product URL', prod_url)
+                prod_dict.set_val(prod_title, 'Product ID', prod_id)
+                prod_dict.set_val(prod_title, 'Variant ID', var_id)
+                prod_dict.set_val(prod_title, 'Cart URL', cart_url)
+
+                if get_auto_checkout(profile_dict):
+                    create_profile(profile_dict, prod_dict, prod_title)
+
+                    if get_new_prod(prod_dict):
+                        main()
+
+        if not prod_dict.is_empty():
+            watch_products(profile_dict, prod_dict, prod_title)
+        else:
+            quit()
+    else:
+        print('call menu')
+
 # Initializing the dictionaries
 profile_dict = Profile()
 prod_dict = Product()
 
 # Banner
-printBanner()
+print_banner() 
 
-### ---------------------------------------------- ###
-
-# Getting the url from the user
-prod_url = input('\nInsert Product URL: ')
-product = getProduct(prod_url)
-
-# Validate URL
-
-prod_title = product['product']['title'] # Product Title
-prod_id = product['product']['id'] # Product ID
-prod_options = product['product']['variants'] # List of product variants
-
-target_prod = getProdType() # Product type
-var_id = getVariantID(prod_options, target_prod) # 
-available = checkAvailability(prod_url, prod_id, var_id)
-cart_url = getCartLink(prod_url, var_id)
-
-product_type = target_prod[0]
-
-### ---------------------------------------------- ###
-
-# If the product is in stock
-if available:
-    print('\n-------- Product in stock --------')
-
-    # Opening cart in browser
-    print('\nAdding product to cart, one moment.')
-    webbrowser.open_new(cart_url)
-    print('Task completed.\n')
-
-    if dict.size() == 0:
-        quit()
-    else: 
-        watchProducts(profile_dict, prod_dict, prod_name)
-
-else:
-    print('\n-------- Product out of stock --------')
-    
-    if getWatchOption():
-        prod_dict.newProduct(prod_title) ## Adding product to watchlist    
-        prod_dict.setVal(prod_title, 'Product URL', prod_url)
-        prod_dict.setVal(prod_title, 'Product ID', prod_id)
-        prod_dict.setVal(prod_title, 'Variant ID', var_id)
-        prod_dict.setVal(prod_title, 'Cart URL', cart_url)
-
-        if getAutoCheckout(profile_dict):
-            createProfile(profile_dict, prod_dict, prod_title)
-
-            if getNewProd(prod_dict):
-                print('run program again') ##########
-
-if prod_dict.size() >= 1:
-    watchProducts(profile_dict, prod_dict, prod_title)
-
-else:
-    quit()
+# Running main program
+main()
